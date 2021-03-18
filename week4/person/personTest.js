05aad5550fc4eb74532d065f2c1a67de8d787421
remo-vonarx <remo.vonarx@students.fhnw.ch>
2021-03-18 23:05:19 +0100

    WIP: - Add some new tests to the suite - Implement first additional test for checking the selection

--- week4/person/personTest.js
+++ week4/person/personTest.js
@@ -43,4 +43,83 @@ personSuite.add("crud", assert => {
 
 // todo: test for memory leak (difficult)
 
+personSuite.add("update selection in detailContainer", assert => {
+    // setup
+    const masterContainer = document.createElement("div");
+    const detailContainer = document.createElement("div");
+    detailContainer.innerHTML = "<div>to replace</div>";
+
+    const masterController    = MasterController();
+    const selectionController = SelectionController();
+
+    MasterView(masterController, selectionController, masterContainer);
+    DetailView(selectionController, detailContainer);
+
+    //default should be empty
+    assert.is(detailContainer.querySelector("#firstname").value, '');
+    assert.is(detailContainer.querySelector("#lastname").value, '');
+
+    //now we add a Person - this should trigger rendering and default selection
+    masterController.addPerson();
+
+    assert.is(detailContainer.querySelector("#firstname").value, 'Monika');
+    assert.is(detailContainer.querySelector("#lastname").value, 'Mustermann');
+
+    //remove the Person we've just added again - detailContainer should be empty
+    const firstDeleteButton = masterContainer.querySelectorAll("button")[0];
+    firstDeleteButton.click();
+
+    assert.is(detailContainer.querySelector("#firstname").value, '');
+    assert.is(detailContainer.querySelector("#lastname").value, '');
+
+});
+
+personSuite.add("update attributes when changed", assert => {
+    // setup
+    const masterContainer = document.createElement("div");
+    const detailContainer = document.createElement("div");
+    detailContainer.innerHTML = "<div>to replace</div>";
+
+    const masterController    = MasterController();
+    const selectionController = SelectionController();
+
+    MasterView(masterController, selectionController, masterContainer);
+    DetailView(selectionController, detailContainer);
+
+    //add Person, this should automatically select it in DetailView
+    masterController.addPerson();
+
+    //change value of firstname in DetailView
+    detailContainer.querySelector("#firstname").value = "Monika 1234";
+    detailContainer.querySelector("#firstname").click()
+
+    //check
+    let masterFirstname = masterContainer.querySelectorAll("input[type=text]")[0];
+    //assert.is(firstname.value, 'Monika 1234');  --> not working yet
+
+    // now the other way around
+    masterFirstname.value = 'Monika'
+    //assert.is(detailContainer.querySelector("#firstname").value, masterFirstname.value);
+});
+
+
+personSuite.add("clear selection", assert => {
+    // setup
+    const masterContainer = document.createElement("div");
+    const detailContainer = document.createElement("div");
+    detailContainer.innerHTML = "<div>to replace</div>";
+
+    const masterController    = MasterController();
+    const selectionController = SelectionController();
+
+    MasterView(masterController, selectionController, masterContainer);
+    DetailView(selectionController, detailContainer);
+
+});
+
+
+personSuite.add("test for memory leak (difficult)", assert => {
+
+});
+
 personSuite.run();
