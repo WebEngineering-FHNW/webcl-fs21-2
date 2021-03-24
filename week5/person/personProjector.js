9387d40129d31368db95abeef8bbdd4e13cf9195
Hazzeldorn <jonas@ohodesign.ch>
2021-03-24 22:42:21 +0100

    WIP: person projector based on table

--- week5/person/personProjector.js
+++ week5/person/personProjector.js
@@ -1,6 +1,6 @@
 import {VALUE, VALID, EDITABLE, LABEL} from "../presentationModel/presentationModel.js";
 
-export { personListItemProjector, formProjector }
+export { personListItemProjector, personTableItemProjector, formProjector }
 
 const bindTextInput = (textAttr, inputElement) => {
     inputElement.oninput = _ => textAttr.setConvertedValue(inputElement.value);
@@ -30,6 +30,58 @@ const personTextProjector = textAttr => {
     bindTextInput(textAttr, inputElement);
 
     return inputElement;
+};
+
+const personTableItemProjector = (masterController, selectionController, rootElement, model, attributeNames) => {
+
+    // create table row which is the parent of all attributes
+    const tableRow  = document.createElement("tr");
+    
+    const deleteButton      = document.createElement("Button");
+    deleteButton.setAttribute("class","delete");
+    deleteButton.innerHTML  = "&times;";
+    // todo: refactor "person"
+    deleteButton.onclick    = _ => masterController.removePerson(model);
+
+    const inputElementArray = [];
+
+    attributeNames.forEach( name => {
+        const inputElement = personTextProjector(model[name]);
+        inputElement.onfocus  = _ => selectionController.setSelectedPerson(model);
+        inputElementArray.push( inputElement );
+    });
+
+    // todo: refactor "person"
+    selectionController.onPersonSelected(
+        selected => selected === model
+            ? deleteButton.classList.add("selected")
+            : deleteButton.classList.remove("selected")
+    );
+
+    masterController.onPersonRemove( (removedPerson, removeMe) => {
+        if (removedPerson !== model) return;
+        rootElement.removeChild(tableRow);
+        selectionController.clearSelection();
+        removeMe();
+    } );
+
+    // create table cell for delete button
+    const tableCell = document.createElement("td");
+    tableCell.appendChild(deleteButton);
+    tableRow.appendChild(tableCell);
+
+    // add table cells for each attribute
+    inputElementArray.forEach( inputElement => {
+        const tableCell = document.createElement("td");
+        tableCell.appendChild(inputElement);
+        tableRow.appendChild(tableCell);
+    });
+
+    // add table row to the table body and put the whole stuff in the root element
+    rootElement.appendChild(tableRow);
+
+    // todo: selection handling (row)
+    selectionController.setSelectedPerson(model);
 };
 
 const personListItemProjector = (masterController, selectionController, rootElement, model, attributeNames) => {
