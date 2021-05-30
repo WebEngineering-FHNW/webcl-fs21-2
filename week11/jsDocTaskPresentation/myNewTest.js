a6cbc369bbdfca4e695e43fa5fc10505faa51345
Joshua Villing <joshua.villing@mimacom.com>
2021-05-30 11:47:44 +0200

    Fix tests

--- week11/jsDocTaskPresentation/myNewTest.js
+++ week11/jsDocTaskPresentation/myNewTest.js
@@ -1,11 +1,12 @@
 import { Suite } from "../test/test.js";
-import { padLeft } from "../util/strings";
+import { padLeft } from "../util/strings.js";
+import "../util/times.js"
 
 const suite = Suite("myNewSuite");
 
 suite.add("myNewTest", assert => {
     const actual = padLeft("123", 5);
-    assert.is(actual, "123  ");
+    assert.is(actual, "  123");
 });
 
 suite.add("myOtherNewTest", assert => {
