7ab9637fc25aec3ff9535cfa856b707e282e1cd3
Joshua Villing <joshua.villing@mimacom.com>
2021-05-30 08:42:32 +0200

    Draft doc for times.js

--- week11/util/times.js
+++ week11/util/times.js
@@ -1,5 +1,26 @@
 // times utility
 
+/**
+ * @callback timesCallback
+ * @param {number|string} i - Number of iteration
+ * */
+
+/**
+ * @typedef {ErrorEvent} NotANumberException
+ * @description An Exception that is thrown when attempting to convert a value that is not a valid numeric expression to a number.
+ */
+
+/**
+ * Repeats a given callback i times.
+ *
+ * This is an extension function to the {string} and {Number} prototypes.
+ * If the function is called on a {string} it is expected, that this string is a valid integer representation
+ *
+ *
+ * @function times
+ * @throws {NotANumberException} - If called on a string that is not a valid integer representation
+ * @param callback
+ */
 const timesFunction = function(callback) {
   if( isNaN(parseInt(Number(this.valueOf()))) ) {
     throw new TypeError("Object is not a valid number");
