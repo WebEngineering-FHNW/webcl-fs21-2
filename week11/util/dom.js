cd3c02b0c04bf810b05df9df6d0063ad4f26c07c
Joshua Villing <joshua.villing@mimacom.com>
2021-05-30 09:15:55 +0200

    Draft doc for dom.js

--- week11/util/dom.js
+++ week11/util/dom.js
@@ -1,6 +1,12 @@
 
 export { dom }
 
+/**
+ * @function dom
+ * @description Creates a new div with the given innerString as content and attaches the created {ChildNode} to the document before returning it.
+ * @param {string }innerString - String representation of the DIV content
+ * @returns {ChildNode} New DIV ChildNode with the given inner content
+ */
 const dom = innerString => {
     const div = document.createElement("DIV");
     div.innerHTML = innerString;
